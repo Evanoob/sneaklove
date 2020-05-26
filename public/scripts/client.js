c1e33f8f8a697cb5c191c3e3839518f209ae7166
Gaelle <mulinia@hotmail.com>
2020-05-26 17:35:09 +0200

    menu burger

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,1 +1,12 @@
 console.log("hello")
+
+const burger = () => {
+    const btnresponsive = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-link");
+
+    btnresponsive.addEventListener('click', () => {
+        btnresponsive.classList.toggle("active");
+        nav.classList.toggle("nav-active")
+    });
+}
+burger()
